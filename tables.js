7b9e56899f20de6d109580936c30ca0355f1f609
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-08-25 12:16:56 -0500

    reservations handling

--- tables.js
+++ tables.js
@@ -1,30 +1,38 @@
-let express = require("express");
-let bodyParser = require("body-parser");
-let path = require("path");
+$(document).ready(function () {
 
-let app = express();
-const PORT = 3000;
+    // let express = require("express");
+    // let bodyParser = require("body-parser");
+    // let path = require("path");
 
-function Table(u_ID, name, phone_num, email){
-    this.u_ID=u_ID;
-    this.name=name;
-    this.phone_num=phone_num;
-    this.email=email;
-};
+    // let app = express();
+    // const PORT = 3000;
+    let tables = [];
+    console.log("its linked");
 
-$("#add-btn").on("click", function(event){
-    event.preventDefault();
-    let newTable =new Table(
-        $("#uniqueid").val().trim(),
-        $("#name").val().trim(),
-        $("#phonenumber").val().trim(),
-        $("#email").val().trim()
-    );
-    tables.push(newTable);
-    console.log(tables);    
-});
-  
-  
+    function Table(u_ID, name, phone_num, email) {
+        this.u_ID = u_ID;
+        this.name = name;
+        this.phone_num = phone_num;
+        this.email = email;
+    };
+
+    $("#add-btn").on("click", function (event) {
+        event.preventDefault();
+        let newTable = new Table(
+            $("#uniqueid").val().trim(),
+            $("#name").val().trim(),
+            $("#phonenumber").val().trim(),
+            $("#email").val().trim()
+        );
+        tables.push(newTable);
+        console.log(tables);
+        $("#uniqueid").val("");
+        $("#name").val("");
+        $("#phonenumber").val("");
+        $("#email").val("");
+    });
+
+
     // if (tables.length > 4) {
     //     tables.push(waitlist);
 
@@ -33,5 +41,4 @@ $("#add-btn").on("click", function(event){
     //     console.log("reservations.html")
     // });
 
-
-
+});
