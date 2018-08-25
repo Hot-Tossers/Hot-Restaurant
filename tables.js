a06ef05808876acf57efd42b34b154fd6ead8d8e
Motherbox <danielohly@gmail.com>
2018-08-25 11:53:03 -0500

    heyboo

---
+++ tables.js
@@ -0,0 +1,37 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let path = require("path");
+
+let app = express();
+const PORT = 3000;
+
+function Table(u_ID, name, phone_num, email){
+    this.u_ID=u_ID;
+    this.name=name;
+    this.phone_num=phone_num;
+    this.email=email;
+};
+
+$("#add-btn").on("click", function(event){
+    event.preventDefault();
+    let newTable =new Table(
+        $("#uniqueid").val().trim(),
+        $("#name").val().trim(),
+        $("#phonenumber").val().trim(),
+        $("#email").val().trim()
+    );
+    tables.push(newTable);
+    console.log(tables);    
+});
+  
+  
+    // if (tables.length > 4) {
+    //     tables.push(waitlist);
+
+    // $.post("/api/tables", newTable)
+    // .then(function(data){
+    //     console.log("reservations.html")
+    // });
+
+
+
