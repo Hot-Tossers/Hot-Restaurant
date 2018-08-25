9e7576fba0918812735395f1cc15575c395d19c8
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-08-25 13:45:26 -0500

    it works

--- tables.js
+++ tables.js
@@ -6,32 +6,32 @@
 
     // let app = express();
     // const PORT = 3000;
-    let tables = [];
-    console.log("its linked");
+    // let tables = [];
+    // console.log("its linked");
 
-    function Table(u_ID, name, phone_num, email) {
-        this.u_ID = u_ID;
-        this.name = name;
-        this.phone_num = phone_num;
-        this.email = email;
-    };
+    // function Table(u_ID, name, phone_num, email) {
+    //     this.u_ID = u_ID;
+    //     this.name = name;
+    //     this.phone_num = phone_num;
+    //     this.email = email;
+    // };
 
-    document.getElementById("#add-btn").addEventListener("click", function (event) {
-        event.preventDefault();
-        let newTable = new Table(
-            document.getElementById("#uniqueid"),
-            document.getElementById("#name"),
-            document.getElementById("#phonenumber"),
-            document.getElementById("#email")
-        );
-        tables.push(newTable);
-        document.getElementById("#uniqueid").innerHTML("");
-        document.getElementById("#name").innerHTML("");
-        document.getElementById("#phonenumber").innerHTML("");
-        document.getElementById("#email").innerHTML("");
-    });
-    module.exports = {
-        tables: () => {
-            return tables;
-        }
-    };
+    // document.getElementById("#add-btn").addEventListener("click", function (event) {
+    //     event.preventDefault();
+    //     let newTable = new Table(
+    //         document.getElementById("#uniqueid"),
+    //         document.getElementById("#name"),
+    //         document.getElementById("#phonenumber"),
+    //         document.getElementById("#email")
+    //     );
+    //     tables.push(newTable);
+    //     document.getElementById("#uniqueid").innerHTML("");
+    //     document.getElementById("#name").innerHTML("");
+    //     document.getElementById("#phonenumber").innerHTML("");
+    //     document.getElementById("#email").innerHTML("");
+    // });
+    // module.exports = {
+    //     tables: () => {
+    //         return tables;
+    //     }
+    // };
